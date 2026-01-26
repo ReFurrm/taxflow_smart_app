47e4ef6357b33241e8704904589c17b853862878
Reese <tmfurr97@icloud.com>
2026-01-26 05:30:03 -0600

    commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
 
-
-// Initialize Supabase client
-// Using direct values from project configuration
-const supabaseUrl = 'https://vifuuyzobgrcmtpuxxfn.supabase.co';
-const supabaseKey = 'sb_publishable_9lSqpifpi9ulkMMkuVGa3w_xQBM0d5k';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-export { supabase };
+NEXT_PUBLIC_SUPABASE_URL=https://olignhedndrxjzlgjvtb.supabase.co
+NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY=sb_publishable_kSY0gixZs5CXD9Nx6hWysA_6LE9xp7w
+SUPBASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9saWduaGVkbmRyeGp6bGdqdnRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNDkxNDUsImV4cCI6MjA4NDcyNTE0NX0.cjznMszpsHOx8Fpxq1MocltWdqN5BguuExZmEV8g1z8
